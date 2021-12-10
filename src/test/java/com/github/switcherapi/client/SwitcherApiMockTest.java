58ed3ea73d7faa0e6f0f8070e52e8531cc7946ab
petruki <31597636+petruki@users.noreply.github.com>
2021-12-10 13:17:14 -0800

    Closes #113

--- src/test/java/com/github/switcherapi/client/SwitcherApiMockTest.java
+++ src/test/java/com/github/switcherapi/client/SwitcherApiMockTest.java
@@ -1,12 +1,12 @@
 package com.github.switcherapi.client;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.io.RandomAccessFile;
