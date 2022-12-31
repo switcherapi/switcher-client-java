bc73364a9d2102e0386f39cea81ff85d5409a23f
petruki <31597636+petruki@users.noreply.github.com>
2022-12-31 11:30:23 -0800

    Closes #213 - Bumped jersey to 2.38

--- src/test/java/com/github/switcherapi/playground/ClientPlayground.java
+++ src/test/java/com/github/switcherapi/playground/ClientPlayground.java
@@ -15,6 +15,7 @@ public class ClientPlayground {
 	public static void test() {
 		configure(ContextBuilder.builder()
 				.contextLocation(Features.class.getCanonicalName())
+				.url("https://switcherapi.com/api")
 				.apiKey("JDJiJDA4JEFweTZjSTR2bE9pUjNJOUYvRy9raC4vRS80Q2tzUnk1d3o1aXFmS2o5eWJmVW11cjR0ODNT")
 				.domain("Playground")
 				.component("switcher-playground"));
