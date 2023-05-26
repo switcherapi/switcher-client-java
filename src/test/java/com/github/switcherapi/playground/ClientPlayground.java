8f8956db6567e6ebffdfdc745edd27804452ee3d
petruki <31597636+petruki@users.noreply.github.com>
2023-05-25 20:08:57 -0700

    Release 2.0.2

--- src/test/java/com/github/switcherapi/playground/ClientPlayground.java
+++ src/test/java/com/github/switcherapi/playground/ClientPlayground.java
@@ -8,10 +8,16 @@ import org.apache.logging.log4j.Logger;
 import com.github.switcherapi.client.ContextBuilder;
 import com.github.switcherapi.client.model.Switcher;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class ClientPlayground {
 	
 	final static Logger logger = LogManager.getLogger(ClientPlayground.class);
-	
+
+	private static final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
 	public static void test() {
 		configure(ContextBuilder.builder()
 				.contextLocation(Features.class.getCanonicalName())
@@ -19,14 +25,15 @@ public class ClientPlayground {
 				.apiKey("JDJiJDA4JEFweTZjSTR2bE9pUjNJOUYvRy9raC4vRS80Q2tzUnk1d3o1aXFmS2o5eWJmVW11cjR0ODNT")
 				.domain("Playground")
 				.component("switcher-playground"));
-		
+
 		initializeClient();
-		
 		Switcher switcher = getSwitcher(MY_SWITCHER);
 		switcher.setShowReason(true);
-		logger.info(switcher.isItOn());
+
+		scheduler.scheduleAtFixedRate(() ->
+				logger.info(switcher.isItOn()), 0, 10, TimeUnit.SECONDS);
 	}
-	
+
 	public static void main(String[] args) {
 		ClientPlayground.test();
 	}
