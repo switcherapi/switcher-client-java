175b51d03720bda381f1f27c0fd46ae886987658
Roger Floriano <31597636+petruki@users.noreply.github.com>
2024-04-22 18:25:13 -0700

    Refactored SwitcherMock to SwitcherTest - Added AB test capability (#284)

---
+++ src/main/java/com/github/switcherapi/client/test/SwitcherTest.java
@@ -0,0 +1,33 @@
+package com.github.switcherapi.client.test;
+
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotate test cases with the Switcher Key and the expected result.
+ * 
+ * <p>
+ * <b>Requires JUnit 5 Jupiter</b>
+ * 
+ * @author Roger Floriano (petruki)
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@ExtendWith(SwitcherTestExtension.class)
+@TestTemplate
+public @interface SwitcherTest {
+
+	String key() default "";
+
+	boolean result() default true;
+
+	boolean abTest() default false;
+
+	SwitcherTestValue[] switchers() default {};
+
+}
