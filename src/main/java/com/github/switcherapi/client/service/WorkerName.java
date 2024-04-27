d38606dbb952b0d4ad272174727c027ae4004945
petruki <31597636+petruki@users.noreply.github.com>
2024-04-26 21:32:04 -0700

    Reverted worker constant name used in v1

--- src/main/java/com/github/switcherapi/client/service/WorkerName.java
+++ src/main/java/com/github/switcherapi/client/service/WorkerName.java
@@ -2,6 +2,7 @@ package com.github.switcherapi.client.service;
 
 public enum WorkerName {
 
+    REGEX_VALIDATOR_WORKER("switcherapi-regex-validator"),
     SNAPSHOT_WATCH_WORKER("switcherapi-snapshot-watcher"),
     SNAPSHOT_UPDATE_WORKER("switcherapi-snapshot-update");
 
