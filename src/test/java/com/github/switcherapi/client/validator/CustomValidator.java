80e71272f3f6296cd2d634c6145d430f3d531be2
Roger Floriano <31597636+petruki@users.noreply.github.com>
2024-11-17 23:16:39 -0800

    Replaced reflection-based impl for validators, impproved SwitcherProperties (#318)
    * Replaced reflection-based impl for validators, impproved SwitcherProperties (#317)
    
    * Replaced reflection-based impl for validators, impproved SwitcherProperties
    
    * chore: replaced with primitive
    
    * Updated RegexValidatorV8

--- src/test/java/com/github/switcherapi/client/validator/CustomValidator.java
+++ src/test/java/com/github/switcherapi/client/validator/CustomValidator.java
@@ -5,10 +5,13 @@ import com.github.switcherapi.client.model.Entry;
 import com.github.switcherapi.client.model.StrategyValidator;
 import com.github.switcherapi.client.model.criteria.Strategy;
 import com.github.switcherapi.client.service.validators.Validator;
-import com.github.switcherapi.client.service.validators.ValidatorComponent;
 
-@ValidatorComponent(type = StrategyValidator.INVALID)
 public class CustomValidator extends Validator {
+
+	@Override
+	public StrategyValidator getType() {
+		return StrategyValidator.INVALID;
+	}
 
 	@Override
 	public boolean process(Strategy strategy, Entry switcherInput)
