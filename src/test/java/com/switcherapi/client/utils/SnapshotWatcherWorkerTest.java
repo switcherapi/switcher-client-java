62b295cd7ddf7769513efaaccbb28acae2018783
petruki <31597636+petruki@users.noreply.github.com>
2025-08-15 20:14:31 -0700

    debug: dangling thread after termination

--- src/test/java/com/switcherapi/client/utils/SnapshotWatcherWorkerTest.java
+++ src/test/java/com/switcherapi/client/utils/SnapshotWatcherWorkerTest.java
@@ -2,6 +2,7 @@ package com.switcherapi.client.utils;
 
 import com.switcherapi.SwitchersBase;
 import com.switcherapi.client.ContextBuilder;
+import com.switcherapi.client.service.WorkerName;
 import com.switcherapi.fixture.CountDownHelper;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -25,8 +26,24 @@ class SnapshotWatcherWorkerTest extends SnapshotTest {
 		CountDownHelper.wait(1);
 		assertWorkerUntil(true, 2);
 
+		Thread.getAllStackTraces().keySet()
+				.forEach(t -> {
+					if (t.getName().equals(WorkerName.SNAPSHOT_WATCH_WORKER.toString())) {
+						System.out.println("Thread: " + t.getName() + " - State: " + t.getState());
+					}
+				});
+
 		SwitchersBase.stopWatchingSnapshot();
-		assertWorkerUntil(false, 15);
+		CountDownHelper.wait(2);
+
+		Thread.getAllStackTraces().keySet()
+				.forEach(t -> {
+					if (t.getName().equals(WorkerName.SNAPSHOT_WATCH_WORKER.toString())) {
+						System.out.println("Thread: " + t.getName() + " - State: " + t.getState());
+					}
+				});
+
+		assertWorkerUntil(false, 10);
 	}
 
 }
