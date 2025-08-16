09d91a278f78773aaf5555257e9a53e7b1b46972
petruki <31597636+petruki@users.noreply.github.com>
2025-08-15 20:34:27 -0700

    debug: dangling thread after termination

--- src/main/java/com/switcherapi/client/SwitcherContextBase.java
+++ src/main/java/com/switcherapi/client/SwitcherContextBase.java
@@ -461,9 +461,18 @@ public abstract class SwitcherContextBase extends SwitcherConfig {
 	 */
 	public static void stopWatchingSnapshot() {
 		if (Objects.nonNull(watcherSnapshot)) {
-			watcherExecutorService.shutdownNow();
-			watcherSnapshot.terminate();
-			watcherSnapshot = null;
+			watcherExecutorService.shutdown();
+			try {
+				if (!watcherExecutorService.awaitTermination(5, TimeUnit.SECONDS)) {
+					watcherExecutorService.shutdownNow();
+				}
+			} catch (InterruptedException e) {
+				watcherExecutorService.shutdownNow();
+				Thread.currentThread().interrupt();
+			} finally {
+				watcherSnapshot.terminate();
+				watcherSnapshot = null;
+			}
 		}
 	}
 	
