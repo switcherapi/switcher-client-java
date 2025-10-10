150f22fc755e1731b28268758e2ca15b000c6197
petruki <31597636+petruki@users.noreply.github.com>
2025-10-09 20:55:13 -0700

    chore: fixes code smell

--- src/main/java/com/switcherapi/client/model/Entry.java
+++ src/main/java/com/switcherapi/client/model/Entry.java
@@ -62,8 +62,7 @@ public class Entry {
 
 	@Override
 	public boolean equals(Object obj) {
-		if (obj instanceof Entry) {
-			Entry entry = (Entry) obj;
+		if (obj instanceof Entry entry) {
 			if (!this.strategy.equals(entry.getStrategy())) {
 				return false;
 			}
