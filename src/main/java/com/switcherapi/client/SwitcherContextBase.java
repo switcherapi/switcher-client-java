d69c1ed0db3bea3126b5e3d4534a1638f4c662ff
petruki <31597636+petruki@users.noreply.github.com>
2026-06-27 15:19:01 -0700

    fix: registerSwitcherKeys to load contextBase from location

--- src/main/java/com/switcherapi/client/SwitcherContextBase.java
+++ src/main/java/com/switcherapi/client/SwitcherContextBase.java
@@ -229,11 +229,13 @@ public abstract class SwitcherContextBase extends SwitcherConfig {
 	 * Register Switcher Keys based on context properties
 	 */
 	private static void registerSwitcherKeys() {
-		if (Objects.nonNull(contextBase)) {
+		final String contextLocation = contextStr(ContextKey.CONTEXT_LOCATION);
+
+		if (Objects.nonNull(contextBase) && contextBase.getClass().getName().equals(contextLocation)) {
 			registerSwitcherKey(contextBase.getClass().getFields());
 		} else {
 			try {
-				final Class<?> clazz = Class.forName(contextStr(ContextKey.CONTEXT_LOCATION));
+				final Class<?> clazz = Class.forName(contextLocation);
 				registerSwitcherKey(clazz.getFields());
 			} catch(ClassNotFoundException e){
 				throw new SwitcherContextException(e.getMessage());
